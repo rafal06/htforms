b1fa86f2c42e0a6774830bedd21fa0a5b85e3ec7
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-05-17 00:23:51 +0200

    Add a link to the source code

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -9,7 +9,7 @@
     <Collapse {isOpen} navbar expand="md">
       <Nav navbar class="ms-auto">
         <NavItem>
-          <NavLink href="#">Source Code</NavLink>
+          <NavLink href="https://github.com/rafal06/htforms">Source Code</NavLink>
         </NavItem>
       </Nav>
     </Collapse>
